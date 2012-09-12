3fe890963594ed9fdcc1ba85a89e1060e518ad32
Tal Liron <tal.liron@gmail.com>
2012-09-12 14:53:37 -0500

    Foundation: fixes to tasks library

--- components/prudence-javascript-library/libraries/scripturian/prudence/tasks.js
+++ components/prudence-javascript-library/libraries/scripturian/prudence/tasks.js
@@ -11,9 +11,9 @@
 // at http://threecrickets.com/
 //
 
+document.executeOnce('/prudence/logging/')
 document.executeOnce('/sincerity/objects/')
 document.executeOnce('/sincerity/json/')
-document.executeOnce('/prudence/logging/')
 
 var Prudence = Prudence || {}
 
@@ -45,33 +45,33 @@ Prudence.Tasks = Prudence.Tasks || function() {
 	/**
 	 * Starts a task in another thread, or even another JVM.
 	 * <p>
-	 * A JavaScript-friendly wrapper over Prudence's application.task and application.distributedTask.
+	 * A JavaScript-friendly wrapper over Prudence's application.task family of APIs.
 	 * <p>
-	 * The argument can be a full params, or two shortcuts: a string (which becomes params.name), or a
+	 * The argument can be a full params, or two shortcuts: a string (which becomes params.uri), or a
 	 * function (which becomes params.fn).
 	 * 
-	 * @param params All params will be merged into the context, as key 'savory.task'
-	 * @param {String} [params.code] The code to execute (you must supply either this or params.name)
-	 * @param {String} [params.name] The document to execute (you must supply either this or params.code)
+	 * @param params All params will be merged into the context, as key 'prudence.task'
+	 * @param {String} [params.uri] The document to execute (you must supply either this, params.code or params.fn)
+	 * @param {String} [params.code] The code to execute (you must supply either this, params.uri or params.fn)
+	 * @param {Function|String} [params.fn] The function to call (you must supply either this, params.code or params.uri)
 	 * @param {String} [params.entryPoint=null] An optional entry point in the document (null or undefined to execute
-	 *        the entire document)
-	 * @param {Function|String} [params.fn] The function to call (if not present, params.name will default
-	 *        to '/savory/call-task-fn/'); will be called with the context as the argument
-	 * @param [params.context] The context to send to the task (will be made available there as document.context,
-	 *        must be serializable for distributed tasks)
+	 *         the entire document); used in conjunction with params.uri
+	 * @param [params.context] The context to send to the task; must be serializable for distributed tasks;
+	 *         will be made available there as document.context, or as a function argument if you are using params.fn
+	 *         or params.entryPoint
 	 * @param {Boolean} [params.json=true] True to serialize params.context into JSON; note that distributed
-	 *        tasks must have a serializable params.context, so it's good practice to always serialize, unless
-	 *        you really need to optimize performance for in-process tasks
+	 *         tasks must have a serializable params.context, so it's good practice to always serialize, unless
+	 *         you really need to optimize performance for in-process tasks
 	 * @param {Boolean} [params.pure=false] True to keep params.context pure, without special additions from this
-	 *        library; implies params.json=false
+	 *         library; implies params.json=false
 	 * @param {Number} [params.block=0] If greater than zero, will block for a maximum of duration in milliseconds
-	 *        waiting for task to finish execution
+	 *         waiting for task to finish execution
 	 * @param {Number} [params.delay=0] The delay in milliseconds before starting the task (ignored for distributed tasks)
 	 * @param {Number} [params.repeatEvery=0] How often in milliseconds to repeat the task (see params.fixedRepeat),
-	 *        zero means the task is executed only once (ignored for distributed tasks)
+	 *         zero means the task is executed only once (ignored for distributed tasks)
 	 * @param {Boolean} [params.fixedRepeat=false] True if repetition should be fixed according to params.repeatEvery,
-	 *        otherwise the delay until the next repetition would begin only when the task finishes an
-	 *        execution
+	 *         otherwise the delay until the next repetition would begin only when the task finishes an
+	 *         execution
 	 * @param {Boolean} [params.distributed=false] True to distribute the task
 	 * @param {String} [params.application] Application's full name (defaults to name of current application)
 	 * @param {Boolean} [params.multi=false] True to distribute task to all members of the cluster
@@ -80,7 +80,7 @@ Prudence.Tasks = Prudence.Tasks || function() {
 	 */
 	Public.task = function(params) {
 		if (Sincerity.Objects.isString(params)) {
-			params = {name: params}
+			params = {uri: params}
 		}
 		else if (typeof params == 'function') {
 			params = {fn: String(params)}
@@ -124,7 +124,8 @@ Prudence.Tasks = Prudence.Tasks || function() {
 				future = application.distributedCodeTask(params.application, params.code, params.context, params.where, params.multi)
 			}
 			else {
-				future = application.distributedExecuteTask(params.application, params.name, params.entryPoint || null, params.context, params.where, params.multi)
+				params.entryPoint = params.entryPoint || null
+				future = application.distributedExecuteTask(params.application, params.uri, params.entryPoint, params.context, params.where, params.multi)
 			}
 		}
 		else {
@@ -135,7 +136,8 @@ Prudence.Tasks = Prudence.Tasks || function() {
 				future = application.codeTask(params.application, params.code, params.context, params.delay, params.repeatEvery, params.fixedRepeat)
 			}
 			else {
-				future = application.executeTask(params.application, params.name, params.entryPoint || null, params.context, params.delay, params.repeatEvery, params.fixedRepeat)
+				params.entryPoint = params.entryPoint || null
+				future = application.executeTask(params.application, params.uri, params.entryPoint, params.context, params.delay, params.repeatEvery, params.fixedRepeat)
 			}
 		}
 		if (params.block) {
@@ -148,28 +150,13 @@ Prudence.Tasks = Prudence.Tasks || function() {
 	/**
 	 * Shortcut to get the document.context and possibly deserialize it.
 	 * 
+	 * @param document The Prudence document
 	 * @param {Boolean} [json=true] True to deserialize params.context from JSON
 	 * @returns The context
 	 */
-	Public.getContext = function(json) {
+	Public.getContext = function(document, json) {
 		json = json === undefined ? true : json
 		return json ? Sincerity.JSON.from(document.context) : document.context
-	}
-	
-	/**
-	 * Evaluates the code as a task, which means that it can execute asynchronously or distributed in the cluster.
-	 * 
-	 * @param {Function|String} code JavaScript closure or code for a closure
-	 * @param [params]
-	 * @see #task
-	 */
-	Public.eval = function(code, params) {
-		params = Sincerity.Objects.merge({
-			name: '/savory/eval/',
-			context: String(code),
-			pure: true
-		}, params)
-		Public.task(params)
 	}
 	
 	return Public
