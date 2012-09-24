7ff336ef510ea80f5e5df30b4ce5019274e02680
Tal Liron <tal.liron@gmail.com>
2012-09-23 22:03:31 -0500

    Display Prudence and Restlet version

--- components/skeleton/component/services/version/default.js
+++ components/skeleton/component/services/version/default.js
@@ -1,4 +1,9 @@
 
+document.executeOnce('/sincerity/jvm/')
+document.executeOnce('/sincerity/templates/')
+
 var version = Sincerity.JVM.fromProperties(Sincerity.JVM.getResourceAsProperties('com/threecrickets/prudence/version.conf'))
 
 component.context.attributes.put('com.threecrickets.prudence.version', version.version)
+
+println('Prudence {0} (Restlet {1} {2})'.cast(version.version, org.restlet.engine.Engine.VERSION, org.restlet.engine.Edition.CURRENT.shortName))
